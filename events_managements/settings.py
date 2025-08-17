9c5613f4bab61ba58835ace805717fc161e2d93f
Abtahi chowdhury <abtahejisan@gmail.com>
2025-08-17 23:08:40 +0600

    renderdatabase

--- events_managements/settings.py
+++ events_managements/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,13 +88,19 @@ WSGI_APPLICATION = 'events_managements.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://mid_term18_user:1wg57trWoLz3dyVEBGuqkoq4ZUsObcH6@dpg-d2h0irndiees73e0vifg-a.oregon-postgres.render.com/mid_term18',
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
