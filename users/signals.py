6e95478e59dcf38d01dc82c7df08dcbe10fb4082
Abtahi chowdhury <abtahejisan@gmail.com>
2025-08-18 00:14:50 +0600

    urlaslash

--- users/signals.py
+++ users/signals.py
@@ -12,7 +12,7 @@ def send_activation_email(sender,instance,created,**kwargs):
     
     if created:
         token = default_token_generator.make_token(instance)
-        activation_url=f"{settings.FRONTEND_URL}/user/activate/{instance.id}/{token}/"
+        activation_url=f"{settings.FRONTEND_URL}/user/activate/{instance.id}/{token}"
         subject='Activate Your Account'
         message=f'Hi {instance.username},\n\n Please activate your account by clicking the link bello:\n{activation_url}\n\nThank You'
         recipient_list=[instance.email]
